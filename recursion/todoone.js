8e6a62e86045f49871e0e4e3cf407515939f1a32
Brandon <brandon@evco.ventures>
2020-08-30 13:26:55 -0500

    completed recursion todoone

---
+++ recursion/todoone.js
@@ -0,0 +1,16 @@
+function sigma() {
+    x = 20;
+    var sum = 0;
+    for (x = 1; x < 20; x++) {
+        sum = (x + 1) + sum;
+        return sum;
+    }
+}
+
+function factorial() {
+    var fact = 5;
+    for (i = 1; i < fact; i++) {
+        n = (i * i++) * n;
+        return n;
+    }
+}
